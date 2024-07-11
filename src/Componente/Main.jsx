da7c645bc07f190df95a1d1e2b78571360cf00b5
ammalgh <ammalgh22@gmail.com>
2024-07-11 15:02:33 +0300

    first commit

--- src/Componente/Main.jsx
+++ src/Componente/Main.jsx
@@ -1,28 +1,28 @@
 
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 
 
 function Main() {
   return (
     <>
-     <div className="flex flex-wrap justify-center items-end p-5 h-[60vh] max-sm:flex-col max-sm:space-y-4 sm:space-y-4 sm:mt-1">
-  <a href="/" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6">
-    <div>Sea</div>
-  </a>
-  <a href="/card2" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6">
-    <div>Desert</div>
-  </a>
-  <a href="/card3" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6">
-    <div>Nature</div>
-  </a>
-  <a href="/card4" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6">
-    <div>Snow</div>
-  </a>
-  <a href="/cards5" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6">
-    <div>Forest</div>
-  </a>
-</div>
+   <div className="flex flex-wrap justify-center items-end p-5 h-[60vh] max-sm:flex-col max-sm:space-y-4 sm:space-y-4 sm:mt-1">
+        <Link to="/" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6 max-sm:w-full">
+          <div>Sea</div>
+        </Link>
+        <Link to="/card2" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6 max-sm:w-full">
+          <div>Desert</div>
+        </Link>
+        <Link to="/card3" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6 max-sm:w-full">
+          <div>Nature</div>
+        </Link>
+        <Link to="/card4" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6 max-sm:w-full">
+          <div>Snow</div>
+        </Link>
+        <Link to="/cards5" className="flex flex-col items-center bg-gray-100 shadow-lg rounded-lg p-10 text-xl m-2 max-sm:p-6 max-sm:w-full">
+          <div>Forest</div>
+        </Link>
+      </div>
 
     
     </>
